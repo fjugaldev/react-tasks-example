72db344f380fe05e52132a7759cdeb482b83fef7
Francisco Ugalde <javierugalde@gmail.com>
2023-02-10 19:23:14 -0500

    initial react tasks example commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primera tarea",
+    description: "Esta es mi primera tarea por hacer",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "Esta es mi segunda tarea por hacer",
+  },
+  {
+    id: 2,
+    title: "Mi tercera tarea",
+    description: "Esta es mi tercera tarea por hacer",
+  },
+];
